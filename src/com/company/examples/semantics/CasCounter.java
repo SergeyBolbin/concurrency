0ef05476ac3e1da594fefc992147ce39eb413394
SergeyBolbin <sergey.bolbin@gmail.com>
2016-10-02 20:48:28 +0300

    cas

---
+++ src/com/company/examples/semantics/CasCounter.java
@@ -0,0 +1,17 @@
+package com.company.examples.semantics;
+
+public class CasCounter {
+    private SimulateCAS value;
+
+    public int getValue() {
+        return value.get();
+    }
+
+    public int increment() {
+        int v;
+        do {
+            v = value.get();
+        } while (v != value.compareAndSwap(v, v + 1));
+        return v + 1;
+    }
+}
---
+++ src/com/company/examples/semantics/SimulateCAS.java
@@ -0,0 +1,22 @@
+package com.company.examples.semantics;
+
+public class SimulateCAS {
+    private int value;
+
+    public synchronized int get() {
+        return value;
+    }
+
+    public synchronized int compareAndSwap(int expectedValue, int newValue) {
+        int oldValue = value;
+        if(oldValue == expectedValue) {
+            value = newValue;
+        }
+
+        return oldValue;
+    }
+
+    public synchronized boolean compareAndSet(int expectedValue, int newValue) {
+        return expectedValue == compareAndSwap(expectedValue, newValue);
+    }
+}
